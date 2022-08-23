fd6160f3c4bad823a3ee756bb87fa2108c017a76
Alan Yang <yangalan114@gmail.com>
2022-08-22 20:22:28 -0400

    Fixing API issues

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://jsmastery-memories.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://jsmastery-memories.herokuapp.com/posts" });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem("profile")) {
